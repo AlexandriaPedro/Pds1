9e95f494685f9d102f92664a259c34f8f61dcce5
Alexandria <alexandriapedroo@gmail.com>
2022-09-12 21:14:32 -0300

    cositas

---
+++ Aulas Práticas/Laboratório 0.1/Imprimindo Números.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int inteiro;
+    float flutuante;
+
+    scanf("%d %f", &inteiro, &flutuante);
+
+    printf("%d\n%.1f", inteiro, flutuante );
+}
