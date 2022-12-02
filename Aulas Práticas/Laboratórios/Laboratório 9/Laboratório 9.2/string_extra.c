d051351d897bcd9327826513bc77b3fb1f7f22d1
Alexandria <alexandriapedroo@gmail.com>
2022-12-02 13:51:29 -0300

    muito chato

--- Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.2/string_extra.c
+++ Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.2/string_extra.c
@@ -1,8 +1,31 @@
 #include "string_extra.h"
 #include <string.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
 // TODO: implemente as funções requeridas
 
 void apaga_caractere(char *str, char c) {
+    int i = 0;
+    roda_caractere(str, c, i);
+}
 
+void roda_caractere(char *str, char c, int i) {
+    if (str[i] != '\0') {
+        verdadeiro_apaga_caractere(str, c, i);
+        roda_caractere(str, c, i + 1);
+
+    } else {
+        exit(0);
+    }
+}
+
+void verdadeiro_apaga_caractere(char *str, char c, int i) {
+    if(str[i] == c) {
+        int nada = 0;
+    }
+    else{
+        printf("%c", str[i]);
+    }
 }
--- Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.2/string_extra.h
+++ Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.2/string_extra.h
@@ -3,4 +3,8 @@
 
 void apaga_caractere(char *str, char c);
 
+void roda_caractere(char *str, char c, int i);
+
+void verdadeiro_apaga_caractere(char *str, char c, int i);
+
 #endif // STRING_EXTRA_H_
