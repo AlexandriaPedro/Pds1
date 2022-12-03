cd3d9544416b0a2156e79c505f5b73817b38a40f
Alexandria <alexandriapedroo@gmail.com>
2022-12-03 20:08:55 -0300

    recursividade

--- Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.3/raiz.c
+++ Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.3/raiz.c
@@ -1,15 +1,23 @@
 #include "raiz.h"
-#include <stdlib.h>
 
 // TODO: implemente as funções requeridas
 
 double raiz(double n, double palpite) {
-    double precisao = 0.01;
+    double estimativa = (palpite * palpite);
 
+    if (absoluto(estimativa - n) < 0.01) {
+        return palpite;
+    }
+
+    if (estimativa < n) {
+        return raiz(n, palpite * 1.5);
+    }
+    
+    return raiz(n, palpite * 0.5);
 }
 
 double absoluto(double a) {
-    a = abs(a);
-    
+    if (a < 0)
+        return a * -1;
     return a;
 }
--- Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.4/palindromo.c
+++ Aulas Práticas/Laboratórios/Laboratório 9/Laboratório 9.4/palindromo.c
@@ -2,4 +2,14 @@
 
 // TODO: implemente as funções requeridas
 
-
+int palindromo(char *palavra, int inicio, int fim) {
+    if (inicio >= fim) {
+        return 1;
+    }
+    
+    if (palavra[inicio] != palavra[fim]) {
+        return 0;
+    }
+    
+    return palindromo(palavra, inicio + 1, fim - 1);
+}
